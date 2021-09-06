3213910b81df05f5a2d8f323e996d98cbef109fc
EmirFederico <emirfederico@gmail.com>
2021-09-06 20:51:44 -0300

    mercadoLiebre con Heroku

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require ("path");
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("servidor corriendo en puerto http://localhost:3000")
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/home.html"));
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/register.html"));
+})
